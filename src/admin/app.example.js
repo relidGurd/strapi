3e5f1195f5a7c5883bcd8ba5a1bf79d29a939837
Skillbox Student <titarevdeniz@yandex.ru>
2023-06-22 18:47:31 +0300

    add ckeditor 5 plugin

--- src/admin/app.example.js
+++ src/admin/app.example.js
@@ -1,33 +1,7 @@
-const config = {
-  locales: [
-    // 'ar',
-    // 'fr',
-    // 'cs',
-    // 'de',
-    // 'dk',
-    // 'es',
-    // 'he',
-    // 'id',
-    // 'it',
-    // 'ja',
-    // 'ko',
-    // 'ms',
-    // 'nl',
-    // 'no',
-    // 'pl',
-    // 'pt-BR',
-    // 'pt',
-    'ru',
-    // 'sk',
-    // 'sv',
-    // 'th',
-    // 'tr',
-    // 'uk',
-    // 'vi',
-    // 'zh-Hans',
-    // 'zh',
-  ],
-};
+import ckeditor5Dll from "ckeditor5/build/ckeditor5-dll.js";
+import ckeditor5MrkdownDll from "@ckeditor/ckeditor5-markdown-gfm/build/markdown-gfm.js";
+
+const config = {};
 
 const bootstrap = (app) => {
   console.log(app);
